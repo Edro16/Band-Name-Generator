1935c80c5a40543c9722837b640d93f4fbed73ce
Edro <9i7DXTw1n2lws1pfFJCmvrRHZsKiP4BHCEYC6VWh@proton.me>
2025-09-30 12:34:18 +0000

    Update BandNameGenerator.py

--- BandNameGenerator.py
+++ BandNameGenerator.py
@@ -1,7 +1,8 @@
-print("Band Name Generator: This program lets you generate a band name using the name of the city that you grew up in and your pet name.")
+print("BandNameGenerator: This program lets you generate a band name using the name of the city that you grew up in and your pet name.")
 print("-- Made by Edro ;) --")
 
 city_name = input("\nWhat's the name of the city that you grew up in?\n")
 pet_name = input("What's the name of your pet\n")
 
 print(f"Your band name could be {city_name + pet_name} or {pet_name + city_name}.")
+
